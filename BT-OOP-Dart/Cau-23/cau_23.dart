19975eea74ac8571fe489ad459c33696417f159e
kats205 <npbaokhanhpro@gmail.com>
2026-03-05 16:54:50 +0700

    Tối ưu, loại bỏ code dư thừa

--- BT-OOP-Dart/Cau-23/cau_23.dart
+++ BT-OOP-Dart/Cau-23/cau_23.dart
@@ -7,17 +7,4 @@ void main() {
   for (var gender in Gender.values) {
     print('- ${gender.name}');
   }
-
-  print('\n--- Test Case Sai ---');
-  try {
-    String input = 'alien';
-    // Cố gắng tìm giá trị enum tương ứng với chuỗi đầu vào sai lệch
-    Gender parsedGender = Gender.values.firstWhere((e) => e.name == input);
-    print('Giá trị tìm thấy: $parsedGender');
-  } catch (e) {
-    print(
-      'Bắt lỗi thành công: Chuỗi đầu vào không khớp với bất kỳ giá trị nào trong Enum Gender.',
-    );
-    print('Chi tiết lỗi hệ thống: $e');
-  }
 }
